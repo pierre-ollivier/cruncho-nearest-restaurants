4fd555519fa4564e345928fc2b596c0ec524ddf4
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2022-11-18 17:17:07 +0100

    Clean the repository and add first API calls (whose results are visible in the web console by inspecting the page)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import './back/main.js';
+import { getAPIResults } from './back/main.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	console.log("Before function call");
+	getAPIResults();
+	console.log(getAPIResults());
+	console.log("After function call");
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>
+					Hello World!
+				</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
--- src/back/main.js
+++ src/back/main.js
@@ -2,20 +2,20 @@ const API_KEY = "AIzaSyDqPP6IuL439Wik7i9T-DIDFCvsMC0pjuM";
 const latitude = 48.89;
 const longitude = 2.33;
 
-const fetchData = async () => {
-    if (latitude !== 0 && longitude !== 0) {
-        const requestOptions = {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
-                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS'
-            }
-        };
-        console.log(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=1500&type=restaurant&key=${API_KEY}`)
-        return fetch("https://whispering-caverns-88652.herokuapp.com/" + `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=1500&type=restaurant&key=${API_KEY}`, requestOptions)
-            .then((response) => response.json())
-            .then((data) => { console.log(data); });
-    }
+console.log("File main.js is reached.");
+
+export function getAPIResults() {
+    const requestOptions = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+        }
+    };
+    console.log(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=1500&type=restaurant&key=${API_KEY}`)
+    return fetch(`https://whispering-caverns-88652.herokuapp.com/` + `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=1500&type=restaurant&key=${API_KEY}`, requestOptions)
+        .then((response) => response.json())
+        .then((data) => { console.log(data); });
 };
